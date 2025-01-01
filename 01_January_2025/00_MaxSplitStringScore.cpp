5aa6ea918b3aa6787df4d29e38494f1715fb0e3d
Shubhansu kumar <shubhansu2021@gmail.com>
2025-01-01 13:34:59 +0530

    Find the max score after splitting string

---
+++ 01_January_2025/00_MaxSplitStringScore.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/maximum-score-after-splitting-a-string/?envType=daily-question&envId=2025-01-01
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    int maxScore(string s) {
+        int rightOnes = 0, leftZeroes = 0;
+        for(char c: s)
+            if(c=='1') rightOnes++;
+        
+        int score = 0;
+        for(int i=0; i<s.length()-1; i++){
+            if(s[i]=='0') leftZeroes++;
+            else rightOnes--;
+            score = max(score, leftZeroes + rightOnes);
+        }
+        return score;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
