2578d075e9bc234afef38499c7898dd2dd71d3c6
Shubhansu kumar <shubhansu2021@gmail.com>
2025-02-07 22:06:26 +0530

    Find the distinct color

---
+++ 02_February_2025/06_DistinctColor.cpp
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/find-the-number-of-distinct-colors-among-the-balls/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        int n = limit+1;
+        vector<int> ans;
+        unordered_map<int, int> mp_balls;
+        unordered_map<int, int> mp_count;
+
+        for(vector<int> &vt: queries){
+            int ball = vt[0];
+            int color = vt[1];
+            if(mp_balls.find(ball) != mp_balls.end()){
+                mp_count[mp_balls[ball]]--;
+                if(mp_count[mp_balls[ball]] == 0){
+                    mp_count.erase(mp_balls[ball]);
+                }
+            }
+            mp_balls[ball] = color;
+            mp_count[color]++;
+
+            ans.push_back(mp_count.size());
+        }
+
+        return ans;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
