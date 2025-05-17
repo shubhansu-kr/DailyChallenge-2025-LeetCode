216408eefcce041cd458b8bc06477393d05d9b9a
shubhansu-kr <shubhansu2021@gmail.com>
2025-05-17 13:34:16 +0530

    Sort Colors

---
+++ 05_May_2025/16_SortColors.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/sort-colors/?envType=daily-question&envId=2025-05-17
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0, mid = 0, high = nums.size()-1;
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+int main () {
+    
+    return 0;
+}
