6544b4b1c8df3b2044e07420127006601597595b
shubhansu-kr <shubhansu2021@gmail.com>
2025-08-01 22:59:43 +0530

    Calculate the pascal's triangle

---
+++ 08_August_2025/00_PascalsTriangle.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/pascals-triangle/?envType=daily-question&envId=2025-08-01
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> r(numRows);
+
+        for (int i = 0; i < numRows; i++) {
+            r[i].resize(i + 1);
+            r[i][0] = r[i][i] = 1;
+    
+            for (int j = 1; j < i; j++)
+                r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+        }
+        
+        return r;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
