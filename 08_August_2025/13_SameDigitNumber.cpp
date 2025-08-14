221cd92131d89851b5c065af2753280e4938051c
shubhansu-kr <shubhansu2021@gmail.com>
2025-08-14 22:08:00 +0530

    Find the largest 3 same digit number in string

---
+++ 08_August_2025/13_SameDigitNumber.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/largest-3-same-digit-number-in-string/?envType=daily-question&envId=2025-08-14
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        int maxi = -1;
+        for(int i = 2; i < num.length(); ++i) {
+            if (num[i] == num[i-1] && num[i] == num[i-2]) maxi = max(maxi, (int)(num[i]-'0'));
+        }
+        if (maxi == -1) return "";
+        string ans = "";
+        for(int i = 0; i < 3; ++i) ans += ('0'+maxi);
+        return ans;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
