2d2a7830490c1367376ec650255afb2d8abdaadd
shubhansu-kr <shubhansu2021@gmail.com>
2025-08-17 21:59:16 +0530

    New 21 Game

---
+++ 08_August_2025/16_New21Game.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/new-21-game/?envType=daily-question&envId=2025-08-17
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        if (k == 0 || n >= k + maxPts) return 1.0;
+        vector<double> dp(n + 1);
+        dp[0] = 1.0;
+        double Wsum = 1.0, res = 0.0;
+        for (int i = 1; i <= n; ++i)
+        {
+            dp[i] = Wsum / maxPts;
+            if (i < k) Wsum += dp[i];
+            else res += dp[i];
+            if (i - maxPts >= 0) Wsum -= dp[i - maxPts];
+        }
+        return res;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
