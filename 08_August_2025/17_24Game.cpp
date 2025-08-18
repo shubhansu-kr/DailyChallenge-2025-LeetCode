ea2e980d0024dc281002e83d35c70ebefb11c7cb
shubhansu-kr <shubhansu2021@gmail.com>
2025-08-18 21:59:13 +0530

    24Game

---
+++ 08_August_2025/17_24Game.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/24-game/?envType=daily-question&envId=2025-08-18
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    bool judgePoint24(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        do {
+            if (valid(nums)) return true;
+        } while(next_permutation(nums.begin(), nums.end()));
+        return false;
+    }
+private:
+    bool valid(vector<int>& nums) {
+        double a = nums[0], b = nums[1], c = nums[2], d = nums[3];
+        if (valid(a+b, c, d) || valid(a-b, c, d) || valid(a*b, c, d) || valid(a/b, c, d)) return true;
+        if (valid(a, b+c, d) || valid(a, b-c, d) || valid(a, b*c, d) || valid(a, b/c, d)) return true;
+        if (valid(a, b, c+d) || valid(a, b, c-d) || valid(a, b, c*d) || valid(a, b, c/d)) return true;
+        return false;
+    }
+    bool valid(double a, double b, double c) {
+        if (valid(a+b, c) || valid(a-b, c) || valid(a*b, c) || b&&valid(a/b, c)) return true;
+        if (valid(a, b+c) || valid(a, b-c) || valid(a, b*c) || c&&valid(a, b/c)) return true;
+        return false;
+    }
+    bool valid(double a, double b) {
+        if (abs(a+b-24.0) < 0.0001 || abs(a-b-24.0) < 0.0001 || abs(a*b-24.0) < 0.0001 || b&&abs(a/b-24.0) < 0.0001) 
+            return true;
+        return false;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
