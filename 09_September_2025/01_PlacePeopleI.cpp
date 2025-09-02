daa34575ecd97d50be604c78fa1b6c3908af870e
shubhansu-kr <shubhansu2021@gmail.com>
2025-09-02 22:21:11 +0530

    place people in ways

---
+++ 09_September_2025/01_PlacePeopleI.cpp
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/?envType=daily-question&envId=2025-09-02
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        sort(points.begin(), points.end(),
+                [](auto &a, auto &b) {
+                    if (a[0] != b[0]) return a[0] < b[0];
+                    return a[1] > b[1];
+                });
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int yi = points[i][1];
+            int maxY = INT_MIN;
+            for (int j = i + 1; j < n; ++j) {
+                int yj = points[j][1];
+                if (yj <= yi && yj > maxY) {
+                    ans++;
+                    maxY = yj;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main () {
+    
+    return 0;
+}
