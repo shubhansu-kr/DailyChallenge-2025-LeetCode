5a42126e720445b6ec5d24cd91a0f092db51b5be
shubhansu-kr <shubhansu2021@gmail.com>
2025-10-06 23:29:13 +0530

    Rising Water

---
+++ 10_October_2025/05_RisingWater.cpp
@@ -0,0 +1,63 @@
+// https://leetcode.com/problems/swim-in-rising-water/?envType=daily-question&envId=2025-10-06
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+
+class Solution {
+public:
+vector<pair<int,int>> directions = {
+    {-1, 0}, {1, 0}, {0, -1}, {0, 1}, 
+};
+    int swimInWater(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+            
+            vector<vector<int>>res(m,vector<int>(n,INT_MAX));
+            if(m==0 || n==0) return 0;
+
+        using Node=pair<int,pair<int,int>>;
+        priority_queue<Node,vector<Node>,greater<Node>>pq;
+
+
+        auto issafe=[&](int x,int y){
+            return x>=0 && x<m && y>=0 && y<n;
+        };
+
+        pq.push({grid[0][0],{0,0}});
+        res[0][0]=grid[0][0];
+        while(!pq.empty()){
+        int d=pq.top().first;
+        pair<int,int>node=pq.top().second;
+        int x=node.first;
+        int y=node.second;
+            if (x==m-1 && y==n-1) return d;
+        pq.pop();
+        for(auto dir :directions){
+            int x_=x+dir.first;
+            int y_=y+dir.second;
+            if(issafe(x_,y_)){
+                int t=max(d,grid[x_][y_]);
+                if(t<res[x_][y_]){
+                    res[x_][y_]=t;
+                    pq.push({t,{x_,y_}});
+                }
+            }   
+        }
+        }
+
+        if(res[m-1][n-1]==INT_MAX) return 0;
+        else return res[m-1][n-1]; 
+    }
+};
+
+int main () {
+    
+    return 0;
+}
